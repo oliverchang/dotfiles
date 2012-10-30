e2d508386311c52ae58485352c8fa4f8c9db2c62
Oliver Chang <oliver.changs@gmail.com>
2012-10-30 11:18:48 +1100

    Clean up vimrc and remove pathogen

--- install.sh
+++ install.sh
@@ -7,11 +7,9 @@ ln -sf `pwd`/zsh/zshrc "$HOME/.zshrc"
 mkdir -p "$HOME/.vim"
 mkdir -p "$HOME/.vim/colors"
 mkdir -p "$HOME/.vim/bundle"
-mkdir -p "$HOME/.vim/autoload"
 
 ln -sf `pwd`/vim/colors/* "$HOME/.vim/colors"
 ln -sf `pwd`/vim/bundle/* "$HOME/.vim/bundle"
-ln -sf `pwd`/vim/autoload/* "$HOME/.vim/autoload"
 
 git submodule init
 git submodule update
