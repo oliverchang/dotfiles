db826b1bd09c9522385acf725859b668b5822976
Oliver Chang <oliver.changs@gmail.com>
2012-10-30 18:12:59 +1100

    Add tmux config

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ ln -sf `pwd`/vim/vimrc "$HOME/.vimrc"
 ln -sf `pwd`/vim/gvimrc "$HOME/.gvimrc"
 ln -sf `pwd`/zsh/zshrc "$HOME/.zshrc"
 
+ln -sf `pwd`/tmux/tmux.conf "$HOME/.tmux.conf"
+
+if [ `uname` = "Darwin" ]; then
+   # Fix copy/paste for OSX
+   ln -sf `pwd`/tmux/tmux.conf.osx "$HOME/.tmux.conf.osx"
+fi
+
 mkdir -p "$HOME/.vim"
 mkdir -p "$HOME/.vim/colors"
 mkdir -p "$HOME/.vim/bundle"
